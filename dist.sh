75470f5c215f301d05aee3363cc9ab4fca6fc59d
Marvin <amazingmarvinapp@gmail.com>
2021-06-22 19:36:38 +0200

    Add dist.sh.

---
+++ dist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git checkout main
+git branch -D production || echo "No local production branch"
+git push --delete origin production || echo "No remote production branch"
+git checkout -b production
+
+yarn run build
+sed -i '.bak' -e 's/^dist//' .gitignore
+rm -f .gitignore.bak
+git add .gitignore dist
+git commit -m "Add dist files."
+git push origin production --force
+git checkout main
