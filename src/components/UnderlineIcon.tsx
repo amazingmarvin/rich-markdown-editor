c9680860eb5e0af1e8657156edd3cfd8e7c14899
Marvin <amazingmarvinapp@gmail.com>
2021-06-30 20:54:06 +0200

    Make underline icon thicker.

--- src/components/UnderlineIcon.tsx
+++ src/components/UnderlineIcon.tsx
@@ -4,7 +4,7 @@ import { Icon } from "outline-icons";
 export default function UnderlineIcon(props) {
   return (
     <Icon {...props}>
-      <path d="M 18 17 H 7 V 4 H 8 V 16 H 17 V 4 H 18 Z M 7 18 H 18 V 19 H 7 Z" />
+      <path d="M 18 16 H 7 V 4 H 9 V 14 H 16 V 4 H 18 Z M 7 17 H 18 V 19 H 7 Z" />
     </Icon>
   );
 }
