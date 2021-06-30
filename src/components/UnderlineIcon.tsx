8cfc75e8ce1f701e677cbf99fea0284e3b0b352d
Marvin <amazingmarvinapp@gmail.com>
2021-06-30 21:04:59 +0200

    Tweak underline icon.

--- src/components/UnderlineIcon.tsx
+++ src/components/UnderlineIcon.tsx
@@ -4,7 +4,7 @@ import { Icon } from "outline-icons";
 export default function UnderlineIcon(props) {
   return (
     <Icon {...props}>
-      <path d="M 18 16 H 7 V 4 H 9 V 14 H 16 V 4 H 18 Z M 7 17 H 18 V 19 H 7 Z" />
+      <path d="M 18 18 H 7 V 4 H 9 V 16 H 16 V 4 H 18 Z M 7 19 H 18 V 21 H 7 Z" />
     </Icon>
   );
 }
