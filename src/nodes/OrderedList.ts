4de5f25c8c068e06364ee9247a367200c6fe1b26
nutdanai-kpjr <nutdanai.kpjr@gmail.com>
2023-01-11 15:49:23 +0700

    Fix ordered list start from zero bug (#891).
    "node.attrs.order" can also be null, which cause the starting number to be 0.
    
    So I've add extra condition check for null value so that it can set starting number to 1.

--- src/nodes/OrderedList.ts
+++ src/nodes/OrderedList.ts
@@ -57,7 +57,8 @@ export default class OrderedList extends Node {
   toMarkdown(state, node) {
     state.write("\n");
 
-    const start = node.attrs.order !== undefined ? node.attrs.order : 1;
+    const order = node.attrs.order;
+    const start = order !== undefined && order !== null ? order : 1;
     const maxW = `${start + node.childCount - 1}`.length;
     const space = state.repeat(" ", maxW + 2);
 
